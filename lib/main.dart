d8872648fbcb8a32689079a24f3c68263a9bf8bb
Prathmesh Choudhari <48889691+iampratham29@users.noreply.github.com>
2020-05-11 20:08:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:scratch/screens/home.dart';
+import 'package:scratch/authfiles/signIn.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: signIN(),
     );
   }
 }
