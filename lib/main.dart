e8c1e3066857f84a0f142923e38f782442a98074
Vedangi Deepak Deshpande <58439293+Vedangi05@users.noreply.github.com>
2020-05-12 09:26:08 +0530

    Debug badge removed
    just a tiny alteration

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,//to remove debug badge
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
